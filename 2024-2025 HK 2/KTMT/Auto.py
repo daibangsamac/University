98f9e4a96fdeb983ceb2eeee81441920d495b8c9
Quang <nb.nhquang@gmail.com>
2025-03-18 16:19:18 +0700

    Data Science Week 6

---
+++ 2024-2025 HK 2/KTMT/Auto.py
@@ -0,0 +1,12 @@
+class AutoCalculating:
+    def binary_to_decimal(x):
+        return 0
+    def decimal_to_binary(x):
+        return 0
+    def decimal_to_hex(x):
+        return 0
+    def decimal_to_byte(x):
+        return 0
+    def decimal_to_quarter(x):
+        return 0
+    
